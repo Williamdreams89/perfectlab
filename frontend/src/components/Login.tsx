07c1373a9bac9d67033f3df6118890c593f73c78
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2024-05-17 20:42:23 +0000

    push bugs

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -5,42 +5,42 @@ import GoogleLogin from "react-google-login";
 
 
 export const Login = () => {
-  const clientId =
-    "676246280208-ulhmdscj2ndbe5k6qo6v5pj5pelep5a0.apps.googleusercontent.com";
+  // const clientId =
+  //   "676246280208-ulhmdscj2ndbe5k6qo6v5pj5pelep5a0.apps.googleusercontent.com";
 
-  const onSuccess = async (res: any) => {
-    console.log("succzddgsfgess:", res.accessToken);
-    const user = {
-      grant_type: "convert_token",
-      client_id: clientId,
-      client_secret: "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",
-      backend: "google-oauth2",
-      token: res.accessToken,
-    };
-    //console.log(user)
-    const { data } = await axios.post(
-      "http://localhost:8000/user/oauth2/convert-token/",
-      user,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+  // const onSuccess = async (res: any) => {
+  //   console.log("succzddgsfgess:", res.accessToken);
+  //   const user = {
+  //     grant_type: "convert_token",
+  //     client_id: clientId,
+  //     client_secret: "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",
+  //     backend: "google-oauth2",
+  //     token: res.accessToken,
+  //   };
+  //   //console.log(user)
+  //   const { data } = await axios.post(
+  //     "http://localhost:8000/user/oauth2/convert-token/",
+  //     user,
+  //     {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
 
-    //console.log(data, data['access_token'])
-    axios.defaults.headers.common[
-      "Authorization"
-    ] = `Bearer ${data["access_token"]}`;
-    localStorage.clear();
-    localStorage.setItem("access_token", data.access_token);
-    localStorage.setItem("refresh_token", data.refresh_token);
-    window.location.href = "/";
-  };
+  //   //console.log(data, data['access_token'])
+  //   axios.defaults.headers.common[
+  //     "Authorization"
+  //   ] = `Bearer ${data["access_token"]}`;
+  //   localStorage.clear();
+  //   localStorage.setItem("access_token", data.access_token);
+  //   localStorage.setItem("refresh_token", data.refresh_token);
+  //   window.location.href = "/";
+  // };
 
-  const onFailure = (err: any) => {
-    console.log("failed:", err);
-  };
+  // const onFailure = (err: any) => {
+  //   console.log("failed:", err);
+  // };
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -69,14 +69,14 @@ export const Login = () => {
     <div className=" flex justify-center items-center flex-col w-[100vw] h-[100vh] absolute bg-white z-50 left-0 top-0">
       <h2>Sign In</h2>
       <div className="w-[30vw] flex flex-col gap-[2rem]">
-        <GoogleLogin
-          clientId={clientId}
+        {/* <GoogleLogin
+          // clientId={clientId}
           buttonText="Sign in with Google"
-          onSuccess={onSuccess}
-          onFailure={onFailure}
+          // onSuccess={onSuccess}
+          // onFailure={onFailure}
           cookiePolicy={"single_host_origin"}
           className=" w-[70%] m-auto text-center"
-        />
+        /> */}
         <form onSubmit={onFormSubmit} className=" w-[90%] flex flex-col gap-[2rem]">
           <div className="w-[88%] m-auto">
             <label htmlFor="email" >Email:</label>
